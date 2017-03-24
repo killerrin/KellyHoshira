a83ce560b3dd359813f4da307bd9454655c53a95
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-03-24 15:09:59 -0400

    Added Bot Config

--- KellyHoshira/KellyHoshiraBot.cs
+++ KellyHoshira/KellyHoshiraBot.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +11,46 @@ namespace KellyHoshira
     public class KellyHoshiraBot
     {
 
+        private DiscordClient m_client;
+        private CommandService m_commandService;
+
         public KellyHoshiraBot()
         {
+            DiscordConfigBuilder config = new DiscordConfigBuilder();
+            config.AppName = "KellyHoshira";
+            config.AppUrl = "https://github.com/killerrin/KellyHoshira";
+            config.AppVersion = "1.0";
+            config.LogLevel = LogSeverity.Info;
+            config.LogHandler = Log;
 
+            m_client = new DiscordClient(config);
+
+            // Setup the Usings
+            m_client.UsingCommands(x => {
+                x.AllowMentionPrefix = true;
+                x.HelpMode = HelpMode.Public;
+            });
+            m_client.GetService<CommandService>();
+
+            // Setup Events
+            m_client.MessageReceived += MessageReceived;
         }
 
         public void Run()
         {
+            m_client.ExecuteAndWait(async () =>
+            {
+                await m_client.Connect("Mjk0ODg5MDU1NjYzOTQ3Nzc2.C7btfQ.MgaU7lzmOMkCGVaXz_3ijHZm6F0", TokenType.Bot);
+            });
+        }
 
+        private void MessageReceived(object sender, MessageEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
         }
     }
 }
