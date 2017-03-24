0b39d09d74e03c4df480850ea2aa134ac3d797f9
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-03-24 17:51:33 -0400

    Added Games

--- KellyHoshira/KellyHoshiraBot.cs
+++ KellyHoshira/KellyHoshiraBot.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using Discord.Commands;
+using FunSharp.Core.Games.Randomized;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,10 +31,150 @@ namespace KellyHoshira
                 x.AllowMentionPrefix = true;
                 x.HelpMode = HelpMode.Public;
             });
-            m_client.GetService<CommandService>();
+            m_commandService = m_client.GetService<CommandService>();
+            SetupGeneralCommands();
+            SetupFunCommands();
 
             // Setup Events
             m_client.MessageReceived += MessageReceived;
+        }
+
+        private void SetupGeneralCommands()
+        {
+            // Standard Commands
+            m_commandService.CreateCommand("greet")
+                .Alias(new string[] { "gr", "hi", "hello" })
+                .Description("Greets a person.")
+                .Parameter("GreetedPerson", ParameterType.Required)
+                .Do(async e =>
+                {
+                    await e.Channel.SendMessage($"Hello, {e.GetArg("GreetedPerson")}");
+                });
+
+            m_commandService.CreateCommand("bye")
+                .Alias(new string[] { "cya", "g2g", "gb", "bb" })
+                .Description("Says 'Bye' to a person")
+                .Parameter("LeavingPerson", ParameterType.Required)
+                .Do(async e =>
+                {
+                    await e.Channel.SendMessage($"See you later, {e.GetArg("LeavingPerson")}");
+                });
+
+            m_commandService.CreateCommand("say")
+                .Alias(new string[] { "echo" })
+                .Description("Echo's back what is said")
+                .Parameter("WhatToSay", ParameterType.Unparsed)
+                .Do(async e =>
+                {
+                    await e.Channel.SendMessage(e.GetArg("WhatToSay"));
+                });
+        }
+        private void SetupFunCommands()
+        {
+            m_commandService.CreateCommand("8ball")
+                .Description("Checks with the Magic 8 Ball... ex: `8ball {question}` OR `8ball {question} -{positive/neutral/negative}`")
+                .Parameter("Question", ParameterType.Unparsed)
+                .Do(async e =>
+                {
+                    var question = e.GetArg("Question");
+
+                    string text = $"{e.User.Mention} asked the 8ball `{question}`";
+                    string flag = "";
+                    string result = "";
+
+                    var questionLower = question.ToLower();
+                    if (questionLower.Contains("-positive"))
+                    {
+                        flag = "positive";
+                        result = $"{Magic8Ball.Instance.RandomPositive()}";
+                    }
+                    else if (questionLower.Contains("-neutral"))
+                    {
+                        flag = "neutral";
+                        result = $"{Magic8Ball.Instance.RandomNeutral()}";
+                    }
+                    else if (questionLower.Contains("-negative"))
+                    {
+                        flag = "negative";
+                        result = $"{Magic8Ball.Instance.RandomNegative()}";
+                    }
+                    else
+                    {
+                        result = $"{Magic8Ball.Instance.RandomAll()}";
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(flag))
+                    {
+                        text += $" with a flag of `{flag}`";
+                    }
+
+                    await e.Channel.SendMessage(text);
+                    await e.Channel.SendMessage(result);
+                });
+
+            m_commandService.CreateCommand("coinToss")
+                .Alias(new string[] { "coin", "coinFlip" })
+                .Description("Tosses a coin ... ex: `coinToss` OR `coinToss -s`")
+                .Parameter("Flag", ParameterType.Optional)
+                .Do(async e =>
+                {
+                    var flag = e.GetArg("Flag").ToLower();
+
+                    await e.Channel.SendMessage($"{e.User.Mention} threw a coin...");
+
+                    CoinResult coinFlip;
+                    if (flag.Equals("-s")) {
+                        coinFlip = Coin.Instance.FlipCoinWithSide();
+                        if (coinFlip == CoinResult.Side)
+                        {
+                            await e.Channel.SendMessage($"The coin landed on its {coinFlip}... wait, that shouldn't have happened");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        coinFlip = Coin.Instance.FlipCoin();
+                    }
+
+                    await e.Channel.SendMessage($"The coin landed on {coinFlip}");
+                });
+
+            m_commandService.CreateCommand("roll")
+                .Description("Rolls Dice ... ex: `roll 3d20 Fire Damage` OR `roll 100`")
+                .Parameter("DieString", ParameterType.Required)
+                .Parameter("Text", ParameterType.Unparsed)
+                .Do(async e =>
+                {
+                    var dieString = e.GetArg("DieString");
+                    var text = e.GetArg("Text");
+
+                    if (!dieString.ToLower().Contains("d"))
+                    {
+                        if (int.TryParse(dieString, out int result))
+                        {
+                            dieString = $"1d{result}";
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    else if (string.IsNullOrWhiteSpace(dieString))
+                    {
+                        dieString = "1d100";
+                    }
+
+                    var dieRolls = Dice.Instance.RollMultiple(dieString);
+                    string rolls = string.Join(", ", dieRolls);
+                    int total = 0;
+                    foreach (var roll in dieRolls)
+                    {
+                        total += roll;
+                    }
+
+                    await e.Channel.SendMessage($"{e.User.Mention} rolled {dieString} {text}");
+                    await e.Channel.SendMessage($"Result: {rolls} | Total: {total} {text}");
+                });
         }
 
         public void Run()
@@ -46,7 +187,7 @@ namespace KellyHoshira
 
         private void MessageReceived(object sender, MessageEventArgs e)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(e.Message);
         }
         private void Log(object sender, LogMessageEventArgs e)
         {
