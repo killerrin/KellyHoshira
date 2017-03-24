6f87f3cafac2bcf6ad31ad4baf2c00b695a14070
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-03-24 13:33:42 -0400

    Initial Commit

---
+++ KellyHoshira/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KellyHoshira
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            KellyHoshiraBot bot = new KellyHoshiraBot();
+            bot.Run();
+        }
+    }
+}
